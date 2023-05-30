ae7a4029ed4a68873f87f560744f1d2e0fd29a54
monster <erelozturk2@gmail.com>
2023-05-30 22:15:41 +0300

    30/05/2023

--- IdeaProjects/APIB129/src/test/java/get_request/Get10.java
+++ IdeaProjects/APIB129/src/test/java/get_request/Get10.java
@@ -24,7 +24,7 @@ public class Get10 extends GoRestBaseUrl {
         And
             The number of users should  be 10
         And
-            We have at least one "active" status
+            We have at least one "active" status.
         And
             "Gov. Vrinda Panicker", "Sen. Devika Embranthiri" and "Rev. Jay Shukla" are among the users
         And
